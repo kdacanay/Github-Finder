fc39c498a2da54d6c1e085bd229fa3369bd141a3
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-06-30 18:34:53 -0400

    added alert, clear button

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import './App.css';
 import axios from 'axios';
+
+
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
   async componentDidMount() {
@@ -32,19 +36,36 @@ class App extends Component {
 
   }
 
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  setAlert = (message, type) => {
+    this.setState({ alert: { message, type } });
+    setTimeout(() => this.setState({ alert: null }), 3000)
+  };
+
+
   render() {
+
+    const { users, loading } = this.state;
+
     return (
       <div className="App">
         {/* these are props passing to Navbar.js  */}
         <Navbar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
   }
 
-}
+};
 
 export default App;
